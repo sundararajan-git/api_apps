f9a69187e4264d2869f9091c29636b71f8ce82cb
sundararajan-git-sfhl <sundararajan.sfhl@gmail.com>
2025-03-21 08:56:14 +0530

    changes in cookies

--- src/utils/generateTokenAndSetCookies.js
+++ src/utils/generateTokenAndSetCookies.js
@@ -10,8 +10,8 @@ export const generateTokenAndSetCookie = async (res, userId, next) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV !== "development",
-      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
+      sameSite: "none",
     });
 
     return token;
